cb2bd88d37add788464bf7ac8e3bf48a2fe821cd
jmachinadev <jmachinadev@gmail.com>
2021-02-25 11:19:30 -0500

    get api running, with single route /info, return movie db

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
 
+// Replace the uri string with your MongoDB deployment's connection string.
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = process.env.MongoConnectionString;
 
+const uri = process.env.MONGO_URI;
 const PORT = 2000;
+const client = new MongoClient(uri, {useNewUrlParser: true});
 const app = express();
 
-async function run() {
+async function run(req, res) {
   try {
     await client.connect();
-
+    console.log(uri)
     const database = client.db('sample_mflix');
     const collection = database.collection('movies');
 
@@ -21,12 +22,16 @@ async function run() {
     const query = { title: 'Back to the Future' };
     const movie = await collection.findOne(query);
 
-    console.log(movie);
+    res.send(movie);
+    // console.log(movie);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+// run().catch(console.dir);
+
+app.get("/info", run);
 
 app.listen(PORT,()=>{
   console.log(`Listening on port ${PORT}`);
