7f5d500da997abed605dd17f0606a7df41ecd7aa
jmachinadev <jmachinadev@gmail.com>
2021-03-01 09:18:06 -0500

    add cors and nodemon dependecy, add param to MongoClient, add catch to async function, fix issue with asyc call to api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
+const cors = require('cors');
 
 // Replace the uri string with your MongoDB deployment's connection string.
 if (process.env.NODE_ENV !== 'production') {
@@ -8,31 +9,28 @@ if (process.env.NODE_ENV !== 'production') {
 
 const uri = process.env.MONGO_URI;
 const PORT = 2000;
-const client = new MongoClient(uri, {useNewUrlParser: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
+app.use(cors());
 
 async function run(req, res) {
   try {
     await client.connect();
-    console.log(uri)
     const database = client.db('sample_mflix');
     const collection = database.collection('movies');
-
-    // Query for a movie that has the title 'Back to the Future'
     const query = { title: 'Back to the Future' };
     const movie = await collection.findOne(query);
 
     res.send(movie);
-    // console.log(movie);
+  } catch (error) {
+    console.error(error);
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.logout();
   }
 }
-// run().catch(console.dir);
+app.get("/info", (req, res, next) => Promise.resolve(run(req, res).catch(next)));
 
-app.get("/info", run);
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
