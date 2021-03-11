bfb28e46828e61e31e08cc3450c058bc09611aab
jmachinadev <jmachinadev@gmail.com>
2021-03-11 14:31:01 -0500

    Add cors dependency create options, add parser to express, add mongoose and create db connection, import routes

--- server.js
+++ server.js
@@ -1,42 +1,73 @@
-// EXPRESS SERVER file
-// run with (node index.js)
-// Express  is used to start the server connection, but it is also
-// a very powerful framework
-const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require('cors');
 
-// Replace the uri string with your MongoDB deployment's connection string.
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+const corsOptions = {
+  // origin: "http://localhost:8081"
+  origin: "*"
+};
 
-const uri = process.env.MONGO_URI;
-const PORT = process.env.PORT || 2000;
-
-// CREATE NEW INSTANCE of SERVER db
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors(corsOptions));
 
-async function run(req, res) {
-  try {
-    await client.connect();
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
-    const query = { title: 'Back to the Future' };
-    const movie = await collection.findOne(query);
+// Start DB
+const db = require('./app/models');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Connected to database!');
+  }).catch((err) => {
+    console.log('Could not connect to DB' + err);
+    process.exit();
+  });
 
-    res.send(movie);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.logout();
-  }
-}
-app.get("/info", (req, res, next) => Promise.resolve(run(req, res).catch(next)));
+app.get('/', (req, res) => {
+  res.send({ message: "Testing UwU" })
+});
 
+// Import ROUTES
+require('./app/routes/post.routes')(app);
+// Listen to DB
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+
+
+// async function run(req, res) {
+// const database = require("./app/models");
+//   const client = database.MongoClient;
+//   try {
+//     await client.connect()
+//     const db = client.db('sample_mflix');
+//     // this code gets collection and inserts new document
+//     const collection = await db.collection('userInfo'); // gets collection
+//     let insertObj = await collection.insertOne(
+//       {
+//         'first_name': 'Robert',
+//         'last_name': 'Snyder',
+//         'zip': 46516
+//       });
+//     //queries first_name
+//     // const query = { first_name: 'Robert' }
+//     // const person = await userInfo.findOne(query);
+//     res.setheader['Content-Type', 'application/json'];
+//     res.send(insertObj);
+//   } catch (err) {
+//     console.error(err);
+//     res.writeHead(400, 'You done messed up A-Aron');
+//     res.write(err.toString());
+//     res.end();
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.logout();
+//   }
+// }
+// app.post("/users", (req, res, next) => Promise.resolve(run(req, res).catch(next)));
+
+
+
